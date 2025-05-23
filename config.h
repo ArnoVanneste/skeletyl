a2a8e18b3e77bb06691c927787f21a48199d3377
ArnoVanneste <arno.fcb@gmail.com>
2025-05-23 16:22:11 +0200

    Make the config miryoku compatible

--- config.h
+++ config.h
@@ -1,5 +1,13 @@
 #pragma once
 
+/* Key matrix configuration. */
+#define MATRIX_ROW_PINS \
+    { GP29, GP26, GP5, GP4 }
+#define MATRIX_COL_PINS \
+    { GP7, GP6, GP21, GP28, GP27 }
+
+#define DIODE_DIRECTION COL2ROW
+
 /* Handedness. */
 #define SPLIT_HAND_PIN GP15
 #define SPLIT_HAND_PIN_LOW_IS_LEFT  // High -> right, Low -> left.
